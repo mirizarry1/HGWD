688df8f7e0c5d952da59789f24ad98da27ae2ee8
Lin Qi <linqi7070@gmail.com>
2019-05-28 12:00:30 -0400

    Audio

---
+++ Humble Goal for World Domination/Assets/_Lin/LSomeTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSomeTrigger : MonoBehaviour {
+
+	// Use this for initialization
+    public AudioSource AS;
+    public bk_AudioManager AM;
+
+	void Start ()
+	{
+	    AS = FindObjectOfType<AudioSource>();
+        AM = FindObjectOfType<bk_AudioManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AS.clip = AM.listOfThemes[1];
+            AS.Play();
+        }
+    }
+}
