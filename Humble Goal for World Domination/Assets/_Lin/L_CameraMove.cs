66a4af63cf3b1d0e604f5bfa46ede94c7c79e1f2
Lin Qi <linqi7070@gmail.com>
2019-06-02 00:00:34 -0400

    Update L_CameraMove.cs

--- Humble Goal for World Domination/Assets/_Lin/L_CameraMove.cs
+++ Humble Goal for World Domination/Assets/_Lin/L_CameraMove.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class L_CameraMove : MonoBehaviour {
-    //Abandoned !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
     protected Transform _XForm_Camera;
@@ -13,7 +12,7 @@ public class L_CameraMove : MonoBehaviour {
     //public Transform _XForm_Parent;
     private Vector3 thisQuat;
     protected Vector3 _LocalRotation;
-    [SerializeField] protected float _CameraDistance = 50f;
+    [SerializeField] protected float _CameraDistance = 22.32f;
 
     public float MouseSensitivity = 4f;
     public float ScrollSensitvity = 2f;
@@ -62,12 +61,12 @@ public class L_CameraMove : MonoBehaviour {
                 else if (_LocalRotation.x < -60f)
                     _LocalRotation.x = -60f;
                     
-                /*
-                if (_LocalRotation.y < 0f)
-                    _LocalRotation.y = 0f;
-                else if (_LocalRotation.y > 90f)
-                    _LocalRotation.y = 90f;
-                    */
+                
+                if (_LocalRotation.y < -45f)
+                    _LocalRotation.y = -45f;
+                else if (_LocalRotation.y > 45f)
+                    _LocalRotation.y = 45f;
+               
             }
             //Zooming Input from our Mouse Scroll Wheel
 
@@ -81,11 +80,16 @@ public class L_CameraMove : MonoBehaviour {
 
             this._CameraDistance += ScrollAmount * -1f;
 
-            this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 10f);
+            this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 30f);
         }
 
         //Actual Camera Rig Transformations
-        Quaternion QT = Quaternion.Euler(_LocalRotation.x, 0, 0);
+
+
+        Quaternion QT = Quaternion.Euler(0, _LocalRotation.y, _LocalRotation.x);
+
+
+
         //Quaternion QT = Quaternion.Euler(_LocalRotation.y , _LocalRotation.x, thisQuat.z);
         //Quaternion QT = Quaternion.Euler(quat.x, _LocalRotation.x, 0);
         this._XForm_Parent.rotation = Quaternion.Lerp(this._XForm_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
@@ -101,9 +105,9 @@ public class L_CameraMove : MonoBehaviour {
         }
         fixedAngle =(Mathf.Clamp(fixedValue,300,360) - 360) / 4.5f;
     
-        this._XForm_Parent.position = new Vector3(0f, fixedAngle, 0);
+        //this._XForm_Parent.position = new Vector3(0f, fixedAngle, 0);
         //float fixAng = (this.transform.parent.eulerAngles.x - 360f);
-        if (this._XForm_Camera.localPosition.z != this._CameraDistance * -1f)
+        if (this._XForm_Camera.localPosition.z != this._CameraDistance )
         {
             Debug.Log("In Process!!!!!");
             //Debug.Log("Angle"+ this.transform.parent.eulerAngles.x);
@@ -111,7 +115,7 @@ public class L_CameraMove : MonoBehaviour {
             
             //Debug.Log("Angle" + fixedAngle);
             //Debug.Log("FixAngle" + fixAng);
-            this._XForm_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._XForm_Camera.localPosition.z, this._CameraDistance * -1f, Time.deltaTime * ScrollDampening));
+            this._XForm_Camera.localPosition = new Vector3(0, Mathf.Lerp(this._XForm_Camera.localPosition.y, this._CameraDistance , Time.deltaTime * ScrollDampening), 0f);
             /*
             if (fixedAngle == 0)
             {
